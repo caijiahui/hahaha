774fb53feabd460da2a7e03a304d19267eb9ac21
caijiahui <jiahui.cai@advantech.com.cn>
2021-01-07 20:12:46 +0800

    exam type

---
+++ advt.CMS/Models/ExamModel/ExamTypeModel.cs
@@ -0,0 +1,21 @@
+﻿using advt.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace advt.CMS.Models.ExamModel
+{
+    public class ExamTypeModel
+    {
+        public ExamType VexamType { get; set; }
+        public ExamTypeModel() : base()
+        {
+            VexamType = new ExamType();
+        }
+        public void SaveType()
+        {
+            var c = Data.ExamType.Update_ExamType(VexamType);
+        }
+    }
+}
