114deeafc9d8840cf1d1f8419d534b02552d1814
caijiahui <jiahui.cai@adavntech.com.cn>
2021-09-17 14:56:28 +0800

    add ...

---
+++ UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2995cc74-0086-40b9-87ff-6696886c7822")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            TBSopPath models = new TBSopPath();
+            try
+            {
+                HttpClient _httpClient = new HttpClient();
+                var parameters = new Dictionary<string, string>();
+                parameters.Add("token", "050b5cc7-0194-4b0b-9a12-9c7fcf458a3a_Q-19568");
+                var byteContent = new ByteArrayContent(System.Text.Encoding.UTF8.GetBytes(Newtonsoft.Json.JsonConvert.SerializeObject(parameters)));
+                byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                var response = _httpClient.PostAsync(" http://job.advantech.com.cn/HRConsole/SSO/ValidateUserFromExam", byteContent);
+                var result = response.Result.Content.ReadAsStringAsync();
+               // var Data = Newtonsoft.Json.JsonConvert.DeserializeObject<QuerySerialNumberPartsResponse>(result.Result);
+                //if (Data != null)
+                //{
+                //    if (Data.Data.Where(x => x.SN == PartSN).Count() != 0)
+                //    {
+                //        PartName = Data.Data.Where(x => x.SN == PartSN).FirstOrDefault().PartNo;
+                //    }
+                //}
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+
+        }
+
+        public class TBSopPath
+        {
+            public string Path { get; set; }
+            public string FileName { get; set; }
+            public string UserName { get; set; }
+            public string Password { get; set; }
+            public bool Flag { get; set; }
+            public string Message { get; set; }
+
+        }
+    }
+}
